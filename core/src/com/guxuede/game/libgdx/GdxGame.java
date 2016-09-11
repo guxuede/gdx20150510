84ae9f67bd55f709318ff73ad6d0be9ed11c6ccc
guxuede <guxuede@gmal.com>
2016-09-11 21:59:43 +0800

    使用Game，screen来封装stage

---
+++ core/src/com/guxuede/game/libgdx/GdxGame.java
@@ -0,0 +1,22 @@
+package com.guxuede.game.libgdx;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.guxuede.game.actor.AnimationEntity;
+
+/**
+ * Created by guxuede on 2016/9/11 .
+ */
+public abstract class GdxGame extends Game {
+
+    public abstract void transferActorToStage(AnimationEntity actor, String stageName, Vector2 pos);
+
+    public void render () {
+        if (screen != null){
+            screen.render(Gdx.graphics.getDeltaTime());
+
+        }
+    }
+
+}
---
+++ core/src/com/guxuede/game/libgdx/GdxScreen.java
@@ -0,0 +1,11 @@
+package com.guxuede.game.libgdx;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by guxuede on 2016/9/11 .
+ */
+public interface GdxScreen extends Screen {
+
+    public void act(float delta) ;
+}
